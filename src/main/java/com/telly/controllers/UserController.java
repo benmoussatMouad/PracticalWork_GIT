a97e8c052ad46282380ca1cef40d752d2baa7b70
MounirCHERIF <mounircherif2001@gmail.com>
2022-01-24 16:45:45 +0100

    Dev2-Affichage reservation

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.telly.controllers;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,17 +29,32 @@ public class UserController {
 
 	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
 	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
-		
+
 		if (result.hasErrors()) {
 			return "reservebus";
 		}
-		
+
 		String username = principal.getName();
 		reserve.getUser().setUsername(username);
-		
+
 		reserveService.reserve(reserve);
-	
-		
+
+
+		return "home";
+
+	}
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+
+
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
+
+
 		return "home";
 
 	}
